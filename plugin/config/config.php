1d1a3b59896d39289c072da2aa195bc7d53cd029
Roger Howard <rogerhoward@mac.com>
2017-04-05 16:11:24 -0700

    Initial plugin

---
+++ plugin/config/config.php
@@ -0,0 +1,3 @@
+<?php
+
+$channel_url = 'https://hooks.slack.com/services/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';
---
+++ plugin/hooks/all.php
@@ -0,0 +1,19 @@
+<?php
+
+function HookSlackAllAfterlogin {
+	$data = array(
+	    'text'      => $username,
+	);   
+
+	$content = json_encode($data);
+
+	$curl = curl_init($channel_url);
+	curl_setopt($curl, CURLOPT_HEADER, false);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_HTTPHEADER,
+	        array("Content-type: application/json"));
+	curl_setopt($curl, CURLOPT_POST, true);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+
+	$json_response = curl_exec($curl);
+}
